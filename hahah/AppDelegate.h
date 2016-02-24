9ba1581e56b9d03d64b936e7b6937250eda2bf6a
student <zhouxu@suonidafahao.local>
2016-02-24 11:26:20 +0800

    Initial Commit

---
+++ hahah/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hahah
+//
+//  Created by zhouxu on 16/2/24.
+//  Copyright © 2016年 NIIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hahah/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hahah
+//
+//  Created by zhouxu on 16/2/24.
+//  Copyright © 2016年 NIIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
