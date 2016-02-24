c530d61b64e4dfaa55f2f94e9e5bbe115c9c0b35
student <zhouxu@suonidafahao.local>
2016-02-24 11:44:47 +0800

    new

---
+++ hahah/gbfdgViewController.h
@@ -0,0 +1,13 @@
+//
+//  gbfdgViewController.h
+//  hahah
+//
+//  Created by zhouxu on 16/2/24.
+//  Copyright © 2016年 NIIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gbfdgViewController : UIViewController
+
+@end
